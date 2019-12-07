9195158acad7310d39be460b7fa29663276f61d9
Beyza Karalı <beyzakarali4743@gmail.com>
2019-12-08 00:34:34 +0300

    Class düzenlemeleri yapıldı.Fonksiyon taslakları hazırlandı ,doldurulacak.

--- ControllerBMS/User.py
+++ ControllerBMS/User.py
@@ -1,16 +1,18 @@
 # created by buzun at 2019-12-03 18:25.
 # email : batuuzun08@gmail.com
+# edited by beyza at 2019-12-05 19:52.
 
 
-from ControllerBMS import Payment, Credit, Account
+from Payment import Payment
+from Credit import Credit
+from Account import Account
+
 
 class User:
 
     #Constuctor function
     def __init__(self, firstName = "", lastName = "", dateOfBirth= "", cityOfBirth= ""
                     , address= "", profilePhoto= "", TCno= 0, phoneNumber= 0, password= ""):
-        #what is the python id? 
-        #static 
         self.__ID = id
         self.__firstName = firstName
         self.__lastName = lastName
@@ -24,24 +26,91 @@ class User:
         self.__payment = Payment()
         self.__account = Account()
         self.__credit = Credit()
-        #generate static id variable, then use setID function.
-        #class tipinde nesne dizisi üretme nasıl yapılır.
+        
+
+     
+     #implement to all set function
+    def setID (self,newID):
+        self.__ID = newID
 
     def setFirstName(self, newName):
         self.__firstName = newName
+
     def setLastName(self, newName):
         self.__lastName = newName
-    
-    #implement to all set function
-    
-    def setTCno(self, TCno):
-        self.__TCno = TCno
 
+    def setdateOfBirth(self, newdateOfBirth):
+        self.__dateOfBirth = newdateOfBirth
+
+    def setcityOfBirth(self, newcityOfBirth):
+        self.__cityOfBirth = newcityOfBirth  
+
+    def setaddress (self, newaddress ):
+        self.__address  = newaddress 
+
+    def setprofilePhoto (self, newprofilePhoto ):
+        self.__profilePhoto = newprofilePhoto      
+
+    def setTCno(self, newTCno):
+        self.__TCno = newTCno    
+
+    def setphoneNumber (self, newphoneNumber ):
+        self.__phoneNumber = newphoneNumber    
+
+    def setpassword (self, newpassword ):
+        self.__password = newpassword  
+
+    def setpayment (self, newpayment  ):
+        self.__payment  = newpayment         
+    
+    def setaccount (self, newaccount  ):
+        self.__account  = newaccount 
+
+    def setcredit (self, newcredit  ):
+        self.__credit = newcredit     
+
+     #implement to all getter function
+
+    def getID (self):
+        return self.__ID 
 
     def getFirstName(self):
         return self.__firstName
-    #implement to all getter function
 
+    def getdateOfBirth(self):
+        return self.__dateOfBirth 
+
+    def getcityOfBirth(self):
+        return self.__cityOfBirth  
+
+    def getLastName(self):
+        return self.__lastName 
+
+    def getaddress (self):
+        return self.__address 
+
+    def getprofilePhoto (self):
+        return self.__profilePhoto 
+
+    def getTCno(self):
+        return self.__TCno       
+
+    def getphoneNumber (self):
+        return self.__phoneNumber
+
+    def getpassword (self):
+        return self.__password      
+    
+    def getpayment (self):
+        return self.__payment   
+    
+    def getaccount (self):
+        return self.__account
+
+    def getcredit (self):
+        return self.__credit  
+
+   
 
 
 
