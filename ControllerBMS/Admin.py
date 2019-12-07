edf045371a0fcda395e09f91928bc5175ddd5056
Beyza Karalı <beyzakarali4743@gmail.com>
2019-12-08 00:33:40 +0300

    Class düzenlemeleri yapıldı.Fonksiyon taslakları hazırlandı ,doldurulacak.

--- ControllerBMS/Admin.py
+++ ControllerBMS/Admin.py
@@ -1,30 +1,53 @@
 # created by buzun at 2019-12-03 19:49.
 # email : batuuzun08@gmail.com
+# edited by beyza at 2019-12-05 20:02.
 
-from ControllerBMS import User
+
+
+from User import User
 
 
 class Admin:
-    #paremetre olarak nesne vermeyi öğren.
+    #learn to export object as a parameter.
     def __init__(self, userName = "", password = 0):
         self.__ID = id
         self.__userName = userName
         self.__password = password
         self.__user = User()
     
-    #super key araştır.
+    #search super key.
 
     #getter and setter functions
+    
+    def setID(self, newID):
+        self.__ID = newID
+   
+    def setuserName(self, newuserName):
+        self.__userName = newuserName    
 
-    def setID():
-        pass
-        #static id değişkeni üret 
+    def setpassword(self, newpassword):
+        self.__password = newpassword
 
-    #veritabanından tüm kullanıcıları listeleyecek.
+    def setuser(self, newuser):
+        self.__user = newuser 
+        
+    def getID(self):
+        return self.__ID 
+
+    def getuserName(self):
+        return self.__userName 
+    
+    def getpassword(self):
+        return self.__password
+
+    def getuser(self):
+        return self.__user     
+
+    #Pull the entire list of users in database.
     def viewAllUser(self):
         pass
 
-    #gelen parametreleri veritabanında karşılaştırıp user döndürecek.
+    #compare the parameters in the database and return user.
     def logIn(self, userName, password):
         pass
 
